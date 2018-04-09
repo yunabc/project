eec4405a0f59eb533002e4899b65837c7f73bcc2
yunabc <450387582@qq.com>
2018-04-09 18:21:17 +0800

    添加 border 1px 样式

--- src/js/index/index.js
+++ src/js/index/index.js
@@ -1,4 +1,4 @@
-import '@css/index.styl';
+// import '@css/common/common.styl';
 
 function *like() {
   yield 5;
@@ -10,3 +10,13 @@ let it = like();
 console.log(it.next());
 
 
+async function like2 (){
+	await 5
+	console.log('test2');
+}
+
+let it2 = like2();
+console.log(it2)
+it2.then(res=>{
+	console.log(res);
+})
--- src/web/pages/orderConfirm/index.js
+++ src/web/pages/orderConfirm/index.js
@@ -1,1 +1,1 @@
-console.log('this is orderConfirm')
+import './index.styl';
